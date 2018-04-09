50a2fa06a1e2ef5e73d7a9ee05103239ee552925
Jose Henrique Barbosa Capibaribe <henriquecapi@hotmail.com>
2018-04-09 12:44:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//fazendo conexao com banco de dados
+try {
+    $conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","nXwww1786");
+   
+    $query = "Select * from clientes where id=:id";
+    
+    $stmt = $conexao->prepare($query);
+
+    $stmt->bindValue(":id",$_GET['id'], PDO::PARAM_INT);
+    //$stmt->bindValue(":id",$_GET['id']);
+
+    $stmt->execute();
+
+    print_r($stmt->fetch(PDO::FETCH_ASSOC));
+
+    //$resultado = $stmt->execute();
+
+   // print_r($resultado);
+   
+}
+catch(\PDOException $e){
+    echo "Não foi possível estabelecer sua conexão!?";
+    echo "<br>Code: ".$e->getCode();
+    echo "<br>Erro: ".$e->getMessage();
+}
